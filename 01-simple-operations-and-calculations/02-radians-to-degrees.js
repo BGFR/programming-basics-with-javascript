da15d32a4cb0b1c4715434bc58d5dfce76859603
Bano Banov <banovb@hotmail.com>
2021-01-09 17:32:34 +0200

    Delete 02-radians-to-degrees.js

--- 01-simple-operations-and-calculations/02-radians-to-degrees.js
+++
@@ -1,8 +0,0 @@
-//02. Radians to Degrees
-function radiansDegrees(input) {
-    let radians = Number(input);
-    let degrees = radians * 180 / Math.PI;
-    console.log(degrees.toFixed(0));
-}
-
-radiansDegrees(0.7854)
