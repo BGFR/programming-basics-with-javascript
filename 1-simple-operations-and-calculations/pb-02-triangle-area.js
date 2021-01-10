e9b38d96902624c23b575496c64d5758c29f1fb3
Bano Banov <banovb@hotmail.com>
2021-01-10 14:06:49 +0200

    Create pb-02-triangle-area.js

---
+++ 1-simple-operations-and-calculations/pb-02-triangle-area.js
@@ -0,0 +1,9 @@
+  //PB-02. Triangle Area
+  function triangle(a, h) {
+    a = Number(a);
+    h = Number(h);
+    area = (a * h) / 2;
+    console.log(area.toFixed(2));
+  }
+  
+  triangle(20, 30);
