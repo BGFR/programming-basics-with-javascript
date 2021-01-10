1b1c431a7565ee689cf03eae8eee56ea6d74a920
Bano Banov <banovb@hotmail.com>
2021-01-10 14:10:52 +0200

    Create pb-09-weather-forecast.js

---
+++ 1-simple-operations-and-calculations/pb-09-weather-forecast.js
@@ -0,0 +1,12 @@
+//PB-09. Weather Forecast
+
+function weather(text) {
+    let weather = text;
+    if (weather === "sunny") {
+        console.log("It's warm outside!");
+    } else {
+        console.log("It's cold outside!");
+    }
+}
+
+weather("sunny");
