89f9498e6aba530fec9869e2a3fc75f98f1d71fc
Bano Banov <banovb@hotmail.com>
2021-01-10 15:30:52 +0200

    Create pb-09-clock.js

---
+++ 4-for-loop/pb-09-clock.js
@@ -0,0 +1,10 @@
+//PB-09. Clock
+
+function clock() {
+    for (let h = 0; h < 24; h++) {
+        for (let m = 0; m < 60; m++) {
+            console.log(`${h} : ${m}`);
+        }
+    }
+}
+clock();
