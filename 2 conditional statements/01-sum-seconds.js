78e6506e06db010fa42f5d445e77d080aec05180
Bano Banov <banovb@hotmail.com>
2021-01-10 14:18:58 +0200

    Create 01-sum-seconds.js

---
+++ 2 conditional statements/01-sum-seconds.js
@@ -0,0 +1,13 @@
+//01. Sum Seconds 
+function solve(timeFirst, timeSecond, timeThird) {
+    timeFirst = Number(timeFirst);
+    timeSecond = Number(timeSecond);
+    timeThird = Number(timeThird);
+    let totalTime = timeFirst + timeSecond + timeThird;
+    let minutes = Math.floor(totalTime / 60);
+    let seconds = totalTime % 60;
+    if (seconds < 10) {
+        console.log(`${minutes}:0${seconds}`);
+    } else { console.log(`${minutes}:${seconds}`) }
+}
+solve("22", "7", "34")
